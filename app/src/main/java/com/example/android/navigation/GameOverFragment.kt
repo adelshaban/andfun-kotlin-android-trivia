b9ff14eac6c69616bd25443e45f807cb8f515823
Adel Shanan <adel.a.shaban@gmail.com>
2022-08-16 04:50:57 +0200

    Step 7

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -20,6 +20,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.Person.fromBundle
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -35,6 +37,10 @@ class GameOverFragment : Fragment() {
         binding.tryAgainButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_gameOverFragment2_to_gameFragment)
         )
+
+        val args : GameOverFragmentArgs = GameOverFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+
 
         return binding.root
     }
